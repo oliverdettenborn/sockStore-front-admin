0eb7bb33b1acdbb472f3a797a27c893ad77d24b8
thaliadettenborn <thalia.born@gmail.com>
2021-01-20 12:12:26 -0300

    add list, edit and create at categories

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Reset } from "styled-reset";
 
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 
 import GlobalStyle from "./styles/Global";
 import authProvider from "./config/authProvider";
 import dataProvider from "./config/dataProvider";
+
+import {
+  CategoriesList,
+  CategoriesEdit,
+  CategoriesCreate,
+} from "./components/Categories";
 
 const App = () => (
   <Router>
     <Reset />
     <GlobalStyle />
     <Admin authProvider={authProvider} dataProvider={dataProvider}>
-      <Resource name="categories" list={ListGuesser} />
+      <Resource
+        name="categories"
+        list={CategoriesList}
+        edit={CategoriesEdit}
+        create={CategoriesCreate}
+      />
     </Admin>
   </Router>
 );
